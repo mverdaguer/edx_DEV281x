7da18930424d5ec47886ca23ab769db06ce180cd
Marcv Verdaguer <marc.verdaguer.ginestera@gmail.com>
2018-07-09 19:29:56 +0200

    Base form (Inputs, select, checkbox).

--- index.js
+++ index.js
@@ -1,132 +1,182 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+ 
+function InputText(props) {
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <input name={props.name} value={props.value} onChange={props.handleChange}/>
+    </div>
+  )
+}
 
-let questions = [
-  {
-    question: 'What is 2 + 2',
-    answers: ['4', '22', '1', '8'],
-    solution: 0
-  },
-  {
-    question: 'What is 2 + 5',
-    answers: ['4', '7', '1', '8'],
-    solution: 1
-  },
-  {
-    question: 'What is 2 + 20',
-    answers: ['4', '22', '1', '8'],
-    solution: 1
-  },
-  {
-    question: 'What is 12 + 2',
-    answers: ['4', '22', '14', '8'],
-    solution: 2
-  },
-  {
-    question: 'What is 2 - 2',
-    answers: ['4', '22', '1', '0'],
-    solution: 3
-  },
-  {
-    question: 'What is 2 / 2',
-    answers: ['4', '22', '1', '8'],
-    solution: 2
+function SelectElement(props) {
+  let options = props.options.map((item) =>
+    <option key={item} value={item}>{item}</option>
+  )
+
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <select name={props.name} value={props.value} onChange={props.handleChange}>
+        {options}
+      </select>
+    </div>
+  )
+}
+
+function CheckboxElement(props) {
+  let options = [];
+  for (let key in props.options) {
+    options.push(<div key={key}>
+      <input value={key} type="checkbox" checked={props.values[key]} onChange={props.handleChekboxChange}/>{props.options[key]}
+    </div>)
   }
-];
 
-class Game extends React.Component{
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      {options}
+    </div>
+  )
+}
 
-  constructor(props) { 
-    super(props)
 
-    this.state = {question: questions[0], correct: 0, incorrect: 0}
-    this.handleClick = this.handleClick.bind(this)
+
+function PostButton(props) {
+  var style = {
+      width:24,
+      height:24
+  }
+  return (
+      <button style = {style} onClick = { () => props.handleClick()}>{props.label}</button>
+  )
+}
+
+function PostText(props) {
+  var style = {
+      border:"1px solid black",
+      width: props.width
+  }
+  return (
+      <div style = {style}>{props.text}</div>
+  )
+}
+
+function Post(props) {
+  var style = {
+      display:"flex"
+  }
+  return (
+      <div style = {style}>
+          <PostButton label = "x" handleClick = {props.removeItem}/>
+          <PostText text = {props.title} width = "200"/>
+      </div>
+  )
+}
+
+function PostList(props) {
+  return (
+      <ol>
+      {
+          props.postList.map((item, index) => 
+              <Post key = {index} 
+                    title = {item.title}
+                    removeItem = {() => props.removeItem(index)}
+              />
+           )
+       }
+      </ol>
+  )  
+}
+
+class App extends React.Component {
+  constructor(props) {
+      super(props)
+      this.state = {checkedValues: {}, items: []}
+      this.handleChange = this.handleChange.bind(this);
+      this.handleChekboxChange = this.handleChekboxChange.bind(this);
+  } 
+
+  handleChange(event) {
+    let target = event.target;
+
+    this.setState((prevState) => {
+      prevState[target.name] = target.value;
+      return prevState;
+    });
+  }
+
+  handleChekboxChange(event) {
+    let target = event.target;
+
+    this.setState((prevState) => {
+      let checkedValues = this.state.checkedValues;
+      checkedValues[target.value] = target.checked;
+
+      prevState.checkedValues = checkedValues;
+      console.log(prevState)
+      return prevState;
+    });
+  }
+
+  addItem() {
+
+      var itemsCopy = this.state.items.slice()
+      var truncatedString = this.state.value.substring(0,20);
+      itemsCopy.push({"title":truncatedString})
+      
+      this.setState({items:itemsCopy, value:""})
   }
   
-  handleClick(index) {
-    this.setState((prevState, props) => {
-      let i = Math.floor((Math.random() * questions.length))
-      if (index === this.state.question.solution) {
-        return {
-          question: questions[i],
-          correct: prevState.correct + 1,
-          incorrect: prevState.incorrect
-        }
-      } else {
-        return {
-          question: questions[i],
-          correct: prevState.correct,
-          incorrect: prevState.incorrect + 1
-        }
-      }
-    })
+  removeItem(index) {
+      var itemsCopy = this.state.items.slice()
+      itemsCopy.splice(index,1);
+      this.setState({items:itemsCopy})
   }
-  
-  render() {
-    var h1Style = {
-      color: 'red',
-      textAlign: 'center'
-    };
 
-    var style = {
-      display: 'flex'
-    };
+  render(){
+    let selectOptions = ["Science Lab", "Swimming", "Cooking", "Painting"];
+    let checkboxOptions = {
+      'a': "Dietary Restrictions",
+      'b': "Physical Disabilities",
+      'c': "Medical Needs"
+    }
 
     return (
-      <div style={style}>
         <div>
-          <h1 style={h1Style}>{this.state.question.question}</h1>
-          <Board answers={this.state.question.answers} handleClick={this.handleClick}/>
+            <InputText title="First Name" 
+                      name="firstName" 
+                      value={this.state.firstName} 
+                      handleChange={this.handleChange}/>
+            <InputText title="Last Name" 
+                      name="lastName" 
+                      value={this.state.lastName} 
+                      handleChange={this.handleChange.bind(this)}/>
+            <SelectElement title="Select Activity"
+                      options={selectOptions}
+                      name="activity"
+                      value={this.state.activity} 
+                      handleChange={this.handleChange.bind(this)}/>
+            <CheckboxElement title="Check all that apply:"
+                      options={checkboxOptions}
+                      name="checkedValues"
+                      values={this.state.checkedValues} 
+                      handleChekboxChange={this.handleChekboxChange.bind(this)}/>
+
+
+            <input value = {this.state.value} onChange = {this.handleChange.bind(this)}/>
+            <button onClick = { () => this.addItem()}>Submit</button>
+            <PostList postList = {this.state.items}
+                      removeItem = {this.removeItem.bind(this)}
+            />
+
         </div>
-        <Solution correct={this.state.correct} incorrect={this.state.incorrect}/>
-      </div>
     )
   }
 }
 
-function Board(props) {
-  var answers = []
-  for (let i = 0; i < 4; i++) {
-    answers.push(<Answer index={i} key={i} text={props.answers[i]} handleClick={props.handleClick}/>)
-  }
-  return (
-    <div>
-      {answers}
-    </div>
-  )
-}
-
-function Answer(props) {
-  var style = {
-    display: 'flex',
-    justifyContent: 'center',
-    width: '300px',
-    height: '50px',
-    color: 'blue'
-  };
-  
-  return (
-    <button style={style} onClick={() => props.handleClick(props.index)}>
-      {props.text}
-    </button>
-  )
-}
-
-function Solution(props) {
-  var style = {
-    'paddingLeft': '50px',
-    'display': 'flex',
-    'flex-direction': 'column',
-    'justify-content': 'center'
-  };
-
-  return (
-    <div style={style}>
-      <h3>Correct: {props.correct}</h3>
-      <h3>Incorrect: {props.incorrect}</h3>
-    </div>
-  )
-}
-
-const root = document.querySelector('#app')
-ReactDOM.render(<Game />, root)
+ReactDOM.render(
+  <App/>,
+  document.getElementById("app")
+)
