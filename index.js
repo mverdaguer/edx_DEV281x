5ad43493ec0846f1dfc0e274508725de82e89bfa
Marcv Verdaguer <marc.verdaguer.ginestera@gmail.com>
2018-07-10 19:58:59 +0200

    Fixing issues, adding table of entries and styling.

--- index.js
+++ index.js
@@ -2,23 +2,41 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
  
 function InputText(props) {
+  const titleStyle = {
+    marginBottom: 0
+  };
+
+  const inputStyle = {
+    height: 30,
+    width: 175
+  };
+
   return (
     <div>
-      <h3>{props.title}</h3>
-      <input name={props.name} value={props.value} onChange={props.handleChange}/>
+      <h3 style={titleStyle}>{props.title}</h3>
+      <input style={inputStyle} name={props.name} value={props.value} onChange={props.handleChange}/>
     </div>
   )
 }
 
 function SelectElement(props) {
-  let options = props.options.map((item) =>
+  const titleStyle = {
+    marginBottom: 0
+  };
+
+  const selectStyle = {
+    height: 30,
+    width: 175
+  };
+
+  const options = props.options.map((item) =>
     <option key={item} value={item}>{item}</option>
   )
 
   return (
     <div>
-      <h3>{props.title}</h3>
-      <select name={props.name} value={props.value} onChange={props.handleChange}>
+      <h3 style={titleStyle}>{props.title}</h3>
+      <select style={selectStyle} name={props.name} value={props.value} onChange={props.handleChange}>
         {options}
       </select>
     </div>
@@ -26,8 +44,12 @@ function SelectElement(props) {
 }
 
 function CheckboxElement(props) {
-  let options = [];
-  for (let key in props.options) {
+  const titleStyle = {
+    marginBottom: 0
+  };
+
+  const options = [];
+  for (const key in props.options) {
     options.push(<div key={key}>
       <input value={key} type="checkbox" checked={props.values[key]} onChange={props.handleChekboxChange}/>{props.options[key]}
     </div>)
@@ -35,71 +57,97 @@ function CheckboxElement(props) {
 
   return (
     <div>
-      <h3>{props.title}</h3>
+      <h3 style={titleStyle}>{props.title}</h3>
       {options}
     </div>
   )
 }
 
+function RemoveButton(props) {
+  const style = {
+    width: 24,
+    height: 24
+  };
 
-
-function PostButton(props) {
-  var style = {
-      width:24,
-      height:24
-  }
   return (
-      <button style = {style} onClick = { () => props.handleClick()}>{props.label}</button>
+    <button style={style} onClick = {() => props.handleClick()}>{props.label}</button>
   )
 }
 
-function PostText(props) {
-  var style = {
-      border:"1px solid black",
-      width: props.width
-  }
+function Registration(props) {
+  const style = {
+    display:"flex"
+  };
+
   return (
-      <div style = {style}>{props.text}</div>
+    <tr>
+      <td><RemoveButton label="x" handleClick={props.removeItem}/></td>
+      <td><b>{props.firstName}</b></td>
+      <td><b>{props.lastName}</b></td>
+      <td><b>{props.activity}</b></td>
+      <td><b>{props.restrictions}</b></td>
+    </tr>
   )
 }
 
-function Post(props) {
-  var style = {
-      display:"flex"
-  }
-  return (
-      <div style = {style}>
-          <PostButton label = "x" handleClick = {props.removeItem}/>
-          <PostText text = {props.title} width = "200"/>
-      </div>
-  )
-}
+function RegistrationList(props) {
+  const tableStyle = {
+    width: 800,
+    marginTop: 10,
+    textAlign: 'center'
+  };
 
-function PostList(props) {
   return (
-      <ol>
-      {
-          props.postList.map((item, index) => 
-              <Post key = {index} 
-                    title = {item.title}
-                    removeItem = {() => props.removeItem(index)}
-              />
-           )
-       }
-      </ol>
+    <table style={tableStyle}>
+      <tbody>
+        <tr>
+          <th>Remove</th>
+          <th>First Name</th> 
+          <th>Last Name</th>
+          <th>Activity</th>
+          <th>Restrictions</th>
+        </tr>
+        {
+        props.registrations.map((item, index) => 
+          <Registration key={index} 
+                firstName={item.firstName}
+                lastName={item.lastName}
+                activity={item.activity}
+                restrictions={item.restrictions}
+                removeItem={() => props.removeItem(index)}
+          />
+          )
+        }
+      </tbody>
+    </table>
   )  
 }
 
 class App extends React.Component {
   constructor(props) {
-      super(props)
-      this.state = {checkedValues: {}, items: []}
-      this.handleChange = this.handleChange.bind(this);
-      this.handleChekboxChange = this.handleChekboxChange.bind(this);
+    super(props)
+
+    this.state = this.initializeState();
+    this.state.items = [];
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChekboxChange = this.handleChekboxChange.bind(this);
   } 
 
+  initializeState() {
+    return {
+      checkedValues: {
+        a: false,
+        b: false,
+        c: false
+      }, 
+      activity: 'Science Lab',
+      firstName: '',
+      lastName: ''
+    };
+  }
+
   handleChange(event) {
-    let target = event.target;
+    const target = event.target;
 
     this.setState((prevState) => {
       prevState[target.name] = target.value;
@@ -108,40 +156,59 @@ class App extends React.Component {
   }
 
   handleChekboxChange(event) {
-    let target = event.target;
+    const target = event.target;
 
     this.setState((prevState) => {
       let checkedValues = this.state.checkedValues;
       checkedValues[target.value] = target.checked;
 
       prevState.checkedValues = checkedValues;
-      console.log(prevState)
       return prevState;
     });
   }
 
   addItem() {
+    let itemsCopy = this.state.items.slice();
 
-      var itemsCopy = this.state.items.slice()
-      var truncatedString = this.state.value.substring(0,20);
-      itemsCopy.push({"title":truncatedString})
-      
-      this.setState({items:itemsCopy, value:""})
+    let newItem = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      activity: this.state.activity,
+      restrictions: ''
+    };
+
+    for (const key in this.state.checkedValues) {
+      if (this.state.checkedValues[key]) {
+        newItem.restrictions += key;
+      }
+    }
+
+    itemsCopy.push(newItem);
+
+    let newState = this.initializeState();
+    newState.items = itemsCopy;
+    this.setState(newState);
   }
   
   removeItem(index) {
-      var itemsCopy = this.state.items.slice()
+      var itemsCopy = this.state.items.slice();
       itemsCopy.splice(index,1);
       this.setState({items:itemsCopy})
   }
 
-  render(){
-    let selectOptions = ["Science Lab", "Swimming", "Cooking", "Painting"];
-    let checkboxOptions = {
+  render() {
+    const buttonStyle = {
+      marginTop: 10,
+      height: 30,
+      width: 175
+    };
+
+    const selectOptions = ["Science Lab", "Swimming", "Cooking", "Painting"];
+    const checkboxOptions = {
       'a': "Dietary Restrictions",
       'b': "Physical Disabilities",
       'c': "Medical Needs"
-    }
+    };
 
     return (
         <div>
@@ -164,13 +231,9 @@ class App extends React.Component {
                       values={this.state.checkedValues} 
                       handleChekboxChange={this.handleChekboxChange.bind(this)}/>
 
-
-            <input value = {this.state.value} onChange = {this.handleChange.bind(this)}/>
-            <button onClick = { () => this.addItem()}>Submit</button>
-            <PostList postList = {this.state.items}
-                      removeItem = {this.removeItem.bind(this)}
-            />
-
+            <button style={buttonStyle} onClick={() => this.addItem()}>Submit</button>
+            <RegistrationList registrations={this.state.items}
+                      removeItem={this.removeItem.bind(this)}/>
         </div>
     )
   }
